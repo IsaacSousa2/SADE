7b75ecdd44ba78bff79701a86e6faa40a28242b3
Isaac Sousa <131274652+IsaacSousa2@users.noreply.github.com>
2023-11-02 19:11:44 -0300

    Cabeçalho concluído!
    (Falta componentização)

--- sadeapp/src/components/header.tsx
+++ sadeapp/src/components/header.tsx
@@ -1,30 +1,42 @@
+'use client'
 import Image from "next/image"
 import logoBranco from "../../public/images/logoBranco.png"
 import menuHamburguer from "../../public/images/menuHamburguer.png"
+import xIcon from "../../public/images/xIcon.png"
+import { useState } from "react";
 export default function Header(){
-
+    const[menuAtivado, setMenuAtivado] = useState(false);
+    function abrirMenu() {
+    
+        if (menuAtivado == false) {
+            setMenuAtivado(true);
+        }
+        else {
+            setMenuAtivado(false);
+        }
+    }
     return(
-
-        <header className="bg-roxoClaro w-full h-14 items-center border-b-white text-center">
+        <header className="bg-roxoClaro w-full h-14 items-center border-b-white text-center" id="Header">
             <div className="w-full p-2 flex justify-between max-w-[1700px] mx-auto">
                 <div className="">
                     <Image src={logoBranco} alt={"Logo Sade Branco"} className=""/>
                 </div>
-                <div className="text-cinza flex-nowrap flex gap-7 items-center md:hidden">
+                <div id="menu" className={`text-cinza flex gap-7 md:gap-20 items-center md:flex-col md:duration-[0.7s] md:right-[0] ${menuAtivado? 'animate-showsidebar md:bg-roxoClaro md:p-4 md:absolute md:w-40 md:h-full md:mr-[-400px]' : 'animate-removesidebar md:hidden md:mt-[-400px]'}}`}>
+                    <button className="items-center hidden md:block justify-center border-#fff border-solid border-2" onClick={abrirMenu}>
+                        <Image src={xIcon} alt={"Ícone para cancelar"}/>
+                    </button>
                     <a href="#" className="hover:text-roxoEscuro duration-[0.5s]">HOME</a>
                     <a href="#" className="hover:text-roxoEscuro duration-[0.5s]">SOBRE</a>
                     <a href="#" className="hover:text-roxoEscuro duration-[0.5s]">DOCUMENTOS</a>
                     <a href="#" className="hover:text-roxoEscuro duration-[0.5s]">PLANOS</a>
                     <a href="#" className="hover:text-roxoEscuro duration-[0.5s]">CONTATO</a>
                 </div>
-                <div className="items-center flex-nowrap flex">
-                    <button className="2xl:hidden">
+                <div className="items-center hidden md:block">
+                    <button className="items-center hidden md:block justify-center border-#fff border-solid border-2 mt-1" onClick={abrirMenu}>
                         <Image src={menuHamburguer} alt={"Menu Hamburguer"}/>
                     </button>
-                </div>
+                </div> 
             </div>
         </header>
-
     )
-
 }
--- sadeapp/tailwind.config.ts
+++ sadeapp/tailwind.config.ts
@@ -30,7 +30,20 @@ const config: Config = {
         'roxoClaro' : '#6948BA',
         'cinza' : '#F0F2F5'
       },
-
+      keyframes: {
+        showSideBar:{
+          '0%': { transform: 'translateY(-400px)', opacity: '0' },
+        '100%': { transform: 'translateY(0px)', opacity: '1' },
+        },
+        removeSideBar:{
+        '100%' :{ transform: 'translateY(0px)', opacity:'1'},
+          '0%' :{ transform: 'translateY(-400)', opacity:'0'},
+        },
+      },
+      animation:{
+        "showsidebar": "showSideBar 0.5s linear",
+        "removesidebar": "removeSideBar 0.5s linear"
+      }
     },
   },
   plugins: [],
